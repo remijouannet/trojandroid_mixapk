0bc099d36de7a65f81d1650c5dbddfa588b7dc5d
hoodlums <hoodlums@hoodlums.com>
2015-03-20 21:08:28 +0100

    commit

--- MixApk.py
+++ MixApk.py
@@ -42,11 +42,16 @@ class ParseArgs:
 
 class ParseManifest:
     def __init__(self, manifest):
+        self.file = manifest
         self.manifest = ET.parse(manifest)
         self.root = self.manifest.getroot()
+        self.application = self.manifest.find('application')
         self.permissions = []
         self.services = []
         self.receiver = []
+        self.nodePermissions = []
+        self.nodeServices = []
+        self.nodeReceiver = []
         self.mainactivity = None
         self.mainpackage = None
 
@@ -80,10 +85,70 @@ class ParseManifest:
 
     def listReceiver(self):
         if len(self.receiver) == 0:
-            for child in self.root.iter('service'):
-                print(child)
+            for child in self.root.iter('receiver'):
                 self.receiver.append(child.get('{http://schemas.android.com/apk/res/android}name'))
         return self.receiver
+
+    def listNodePermissions(self):
+        if len(self.nodePermissions) == 0:
+            for child in self.root.iter('uses-permission'):
+                self.nodePermissions.append(child)
+        return self.nodePermissions
+
+    def listNodeService(self):
+        if len(self.nodeServices) == 0:
+            for child in self.root.iter('service'):
+                self.nodeServices.append(child)
+        return self.nodeServices
+
+    def listNodeReceiver(self):
+        if len(self.nodeReceiver) == 0:
+            for child in self.root.iter('receiver'):
+                self.nodeReceiver.append(child)
+        return self.nodeReceiver
+
+
+class EditManifest(ParseManifest):
+    def __init__(self, manifest):
+        ParseManifest.__init__(self, manifest=manifest)
+
+    def write(self):
+        self.manifest.write(self.file)
+
+    def addService(self, node):
+        if type(node) is list:
+            for service0 in node:
+                change = True
+                for service1 in self.listService():
+                    if service0.get('{http://schemas.android.com/apk/res/android}name') == service1:
+                        change = False
+                if change:
+                    self.application.append(service0)
+        else:
+            for service in self.listService():
+                if service == node.get('{http://schemas.android.com/apk/res/android}name'):
+                    return
+            self.application.append(node)
+        self.write()
+
+    def addReceiver(self, node):
+        pass
+
+    def addPermissions(self, node):
+        if type(node) is list:
+            for permission0 in node:
+                change = True
+                for permission1 in self.listPermissions():
+                    if permission0.get('{http://schemas.android.com/apk/res/android}name') == permission1:
+                        change = False
+                if change:
+                    self.root.append(permission0)
+        else:
+            for permission in self.listPermissions():
+                if permission == node.get('{http://schemas.android.com/apk/res/android}name'):
+                    return
+            self.root.append(node)
+        self.write()
 
 
 def error(message, ex, code):
@@ -112,18 +177,23 @@ else:
 
 try:
     print('pass apktool')
-    # call(apktool + " d -v -f -o " + apk1Directory + " " + apk1, shell=True)
-    # call(apktool + " d -v -f -o " + apk2Directory + " " + apk2, shell=True)
+    call(apktool + " d -v -f -o " + apk1Directory + " " + apk1, shell=True)
+    call(apktool + " d -v -f -o " + apk2Directory + " " + apk2, shell=True)
 except OSError as ex:
     error("can't extract the two apk", str(ex), 1)
 
 ParseManifest1 = ParseManifest(manifest=apk1Manifest)
 ParseManifest2 = ParseManifest(manifest=apk2Manifest)
 
+EditManifest1 = EditManifest(manifest=apk1Manifest)
+EditManifest2 = EditManifest(manifest=apk2Manifest)
+
+#shutil.rmtree(apk2Smali + ParseManifest2.findMainPackage().replace('.', '/') + '/' + packageToInject.split('.').pop() + '/')
+
+
 apk1Action = apk1Smali + packageToInject.replace('.', '/') + '/'
 apk2Action = apk2Smali + ParseManifest2.findMainPackage().replace('.', '/') + '/' + packageToInject.split('.').pop() + '/'
 
-shutil.rmtree(apk2Smali + ParseManifest2.findMainPackage().replace('.', '/') + '/' + packageToInject.split('.').pop() + '/')
 shutil.copytree(apk1Smali + packageToInject.replace('.', '/') + '/',
             apk2Smali + ParseManifest2.findMainPackage().replace('.', '/') + '/' + packageToInject.split('.').pop() + '/')
 
@@ -131,3 +201,5 @@ for file in glob.glob(apk2Action + '*.smali'):
     sed(file, ParseManifest1.findMainPackage().replace('.', '/'), ParseManifest2.findMainPackage().replace('.', '/'))
 
 
+EditManifest2.addPermissions(ParseManifest1.listNodePermissions())
+EditManifest2.addService(ParseManifest1.listNodeService())
